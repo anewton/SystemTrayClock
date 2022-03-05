47803c51581a2ece991fd7953c13babcd360da37
Allen <1723400+anewton@users.noreply.github.com>
2022-03-05 13:29:27 -0800

    Updated buildInstaller to exit with success or fail

--- buildInstaller.bat
+++ buildInstaller.bat
@@ -1,5 +1,8 @@
+echo off
 set devenvPath=%1
 set slnPath=%2
 set projPath=%3
 set config=%4
-%devenvPath% %slnPath% /Project %projPath% /Build %config% /Out "installer.log" > nul if ERRORLEVEL 1 echo Error %ERRORLEVEL% else echo 0
+%devenvPath% %slnPath% /Project %projPath% /Rebuild %config%
+if ERRORLEVEL 1 exit 1
+exit 0
