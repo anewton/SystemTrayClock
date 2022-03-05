6238719fb337257f450d338710f14e90dbec4f2f
Allen <1723400+anewton@users.noreply.github.com>
2022-03-05 14:25:40 -0800

    Fix bat file

--- outOfProcFix.bat
+++ outOfProcFix.bat
@@ -3,7 +3,7 @@ SET current_path="%CD%"
 ECHO Setting the current path to the DisableOutOfProcBuild.exe installation folder.
 CD "%vsIDEPath%\CommonExtensions\Microsoft\VSI\DisableOutOfProcBuild"
 ECHO:
-CALL DisableOutOfProcBuild.exe
+START /d DisableOutOfProcBuild.exe
 ECHO: & ECHO Revert the previous current directory.
 CD %current_path%
 ECHO CD is set to %CD%
