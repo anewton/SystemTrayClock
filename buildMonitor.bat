ae9c429e7233eb649e5aa64a876e9a34cc53fab7
Allen <1723400+anewton@users.noreply.github.com>
2022-03-06 15:08:49 -0800

    Update buildMonitor.bat

--- buildMonitor.bat
+++ buildMonitor.bat
@@ -3,16 +3,17 @@ set devenvPath=%1
 set slnPath=%2
 set projPath=%3
 set config=%4
+set command="%devenvPath% %slnPath% /Project %projPath% /Rebuild %config%"
+echo %command%
 setlocal DisableDelayedExpansion
-FOR /F "tokens=*" %%a in ('"%devenvPath%" "%slnPath%" /Project "%projPath%" /Rebuild "%config%" ^|findstr /n $') do (
+FOR /F "tokens=*" %%a in ('%command% ^|findstr /n $') do (
   set "line=%%a"
   setlocal EnableDelayedExpansion
   set "line=!line:*:=!"
   echo(!line!
   echo %%(!line!|find "2 succeeded, 0 failed, 0 skipped" >nul
   if NOT errorlevel 1 (
-	  taskkill /f /im "devenv.com"
+	  rem taskkill /f /im "devenv.com"
 	  exit 0
   )
   endlocal
-)
