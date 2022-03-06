5fd8b4adbacc601993c44c5265134694814f8c3d
Allen <1723400+anewton@users.noreply.github.com>
2022-03-06 15:55:51 -0800

    Fixing buildMonitor.bat

--- buildMonitor.bat
+++ buildMonitor.bat
@@ -3,7 +3,7 @@ set devenvPath=%1
 set slnPath=%2
 set projPath=%3
 set config=%4
-set command="%devenvPath% %slnPath% /Project %projPath% /Rebuild %config%"
+set command=%devenvPath% %slnPath% /Project %projPath% /Rebuild %config%
 echo %command%
 setlocal DisableDelayedExpansion
 FOR /F "tokens=*" %%a in ('%command% ^|findstr /n $') do (
@@ -13,8 +13,8 @@ FOR /F "tokens=*" %%a in ('%command% ^|findstr /n $') do (
   echo(!line!
   echo %%(!line!|find "2 succeeded" >nul
   if NOT errorlevel 1 (
-	  rem taskkill /f /im "devenv.com"
 	  exit 0
   )
   endlocal
 )
+exit 0
