6eea45317c4dd1139d41bb61c94071c099bb9888
Allen <1723400+anewton@users.noreply.github.com>
2022-03-06 12:55:02 -0800

    Exit batch file with build is complete

--- buildInstaller.bat
+++ buildInstaller.bat
@@ -3,6 +3,11 @@ set devenvPath=%1
 set slnPath=%2
 set projPath=%3
 set config=%4
-%devenvPath% %slnPath% /Project %projPath% /Rebuild %config%
-if ERRORLEVEL 1 exit 1
-exit 0
+%devenvPath% %slnPath% /Project %projPath% /Rebuild %config% > log.txt
+find "========== Rebuild All: 2 succeeded, 0 failed, 0 skipped ==========" log.txt && (
+  echo "Success"
+  exit 0
+) || (
+  echo "Error"
+  exit 1
+)
